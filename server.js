f37354fdd641f531cb4738f8dac73a9f63980640
Jacques <jacques.scoffoni@gmail.com>
2023-08-18 03:08:26 -0400

    Post route addition and debugging

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-
+const { v4: uuidv4 } = require("uuid");
 
 // Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use(express.json());
 
 // Homepage route
 app.get("/", (req, res) => {
@@ -20,17 +21,36 @@ app.get("/notes", (req, res) => {
 });
 
 // Existing notes
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Failed to read notes' });
-        return;
-      }
-      const notes = JSON.parse(data);
-      res.json(notes);
-    });
-  });  
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Failed to read notes" });
+      return;
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+// Add a note to the existing ones
+app.post("/api/notes", (req, res) => {
+  const existingNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+  existingNotes.push(newNote);
+  fs.writeFileSync(
+    "./db/db.json",
+    JSON.stringify(existingNotes, null, 2),
+    "utf8"
+  );
+
+  res.json(newNote);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
