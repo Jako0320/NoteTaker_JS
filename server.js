807c4955247cf2d9f3041036435dda64fcd05415
Jacques <jacques.scoffoni@gmail.com>
2023-08-18 02:41:43 -0400

    Directory corrections and cleanup

--- server.js
+++ server.js
@@ -2,23 +2,26 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
+
 // Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
 // Homepage route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // Notes route
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // Existing notes
 app.get('/api/notes', (req, res) => {
-    fs.readFile('db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
         res.status(500).json({ error: 'Failed to read notes' });
@@ -27,13 +30,7 @@ app.get('/api/notes', (req, res) => {
       const notes = JSON.parse(data);
       res.json(notes);
     });
-  });
-  
-
-// 404 error route
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/404.html"))
-);
+  });  
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
